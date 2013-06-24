83922ba4bf83b9dc2de0833366f672b03bdde383
Magnus Härlin <magnus.harlin@gmail.com>
2013-06-24 21:44:19 +0200

    Added knockout bindings to Index page with auction list

---
+++ Sommarkollo2013/SpaDemo/Scripts/ViewModels/Auction.js
@@ -0,0 +1,23 @@
+﻿var Auction = function (description, image, seller, title, url) {
+	this.description = ko.observable(description);
+	this.image = ko.observable(image);
+	this.seller = ko.observable(seller);
+	this.title = ko.observable(title);
+	this.url = ko.observable(url);
+};
+
+var auctionViewModel = {
+	auctions: ko.observableArray([])
+};
+
+$(document).ready(function() {
+	$.getJSON('/api/auction', function(data) {
+		auctionViewModel = ko.mapping.fromJS(data);
+		ko.applyBindings(auctionViewModel);
+	});
+});
+//Description = "The best lego ever made",
+//								Image = "~/Images/lego.jpg",
+//								Seller = "Kalle",
+//								Title = "Lego auction",
+//								Url = "~/Home/Auction/1"
