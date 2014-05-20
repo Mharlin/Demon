ad25423e8f991a58f0745b43eed8044daf9bcc00
Magnus Härlin <magnus.harlin@gmail.com>
2014-05-20 14:42:38 +0200

    Devsum 2014 - SortByName with key

--- Devsum2014/TypeScriptHTMLApp1/app.js
+++ Devsum2014/TypeScriptHTMLApp1/app.js
@@ -1,8 +1,24 @@
-﻿function sortByName(a) {
+﻿var brands = [
+    { name: "Apple", value: 400 },
+    { name: "Microsoft", value: 700 },
+    { name: "Dell", value: 200 }
+];
+
+function sortByName(a, keyOf) {
     var result = a.slice(0);
     result.sort(function (x, y) {
-        return x.name.localCompare(y.name);
+        var kx = keyOf(x);
+        var ky = keyOf(y);
+
+        return kx > ky ? 1 : kx < ky ? -1 : 0;
     });
     return result;
 }
+
+var sorted = sortByName(brands, function (x) {
+    return x.value;
+});
+var name = sorted[0].name;
+
+document.body.innerText = JSON.stringify(sorted, null, 4);
 //# sourceMappingURL=app.js.map
--- Devsum2014/TypeScriptHTMLApp1/app.ts
+++ Devsum2014/TypeScriptHTMLApp1/app.ts
@@ -1,7 +1,25 @@
-﻿function sortByName(a) {
+﻿var brands = [
+    { name: "Apple", value: 400 },
+    { name: "Microsoft", value: 700 },
+    { name: "Dell", value: 200 }
+];
+
+interface IName {
+    name: string;
+}
+
+function sortByName<T extends IName>(a: T[], keyOf: (item: T) => any) {
     var result = a.slice(0);
-    result.sort(function(x, y) {
-        return x.name.localCompare(y.name);
+    result.sort((x, y) => {
+        var kx = keyOf(x);
+        var ky = keyOf(y);
+
+        return kx > ky ? 1 : kx < ky ? -1 : 0;
     });
     return result;
 }
+
+var sorted = sortByName(brands, x => x.value);
+var name = sorted[0].name;
+
+document.body.innerText = JSON.stringify(sorted, null, 4);
