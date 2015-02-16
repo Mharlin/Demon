fa2546cce08394c18722bc94d6804afd3379f037
Magnus Härlin <magnus.harlin@gmail.com>
2015-02-16 21:34:19 +0100

    F# Eye for the C# guy Created F# project with json type provider

---
+++ NForum2015/FSharpEyeForTheCSharpGuy/TypeProvider/Scripts/load-references.fsx
@@ -0,0 +1,9 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#r @"..\..\packages\FSharp.Data.2.1.1\lib\net40\FSharp.Data.dll"
+#r @"System.dll"
+#r @"System.Core.dll"
+#r @"System.Numerics.dll"
+#r @"System.Xml.Linq.dll"
+#r @"..\..\packages\FSharp.Charting.0.90.9\lib\net40\FSharp.Charting.dll"
+#r @"System.Drawing.dll"
+#r @"System.Drawing.Design.dll"
---
+++ NForum2015/FSharpEyeForTheCSharpGuy/TypeProvider/TypeProviders.fs
@@ -0,0 +1,44 @@
+﻿namespace TypeProvider
+
+module JsonParse =
+
+    open FSharp.Charting
+    open FSharp.Data
+
+    //Access by strongly typed object by property name
+    type Simple = JsonProvider<""" { "name": "Magnus", "age":35 }  """>
+    let simple = Simple.Parse(""" { "name": "Tove", "age": 4 } """ )
+
+    //Type inferred
+    type Numbers = JsonProvider<""" [1, 2, 3, 3.14] """>
+    let nums = Numbers.Parse(""" [2, 1.2, 45.1, 98.2, 5] """)
+    let total = nums |> Seq.sum
+
+    //Mixed types
+    type Mixed = JsonProvider<""" [1, 2, "hello", "world"] """>
+    let mixed = Mixed.Parse(""" [4, 5, "hello", "world" ] """)
+//    mixed.Numbers |> Seq.sum
+//    mixed.Strings |> String.concat ", "
+
+    //GitHub issues
+//    type GitHub = JsonProvider<"https://api.github.com/repos/fsharp/FSharp.Data/issues">
+    type GitHub = JsonProvider<"https://api.github.com/repos/Mharlin/Demon/issues">
+
+    let topRecentlyUpdatedIssues = 
+        GitHub.GetSamples()
+        |> Seq.filter (fun issue -> issue.State = "open")
+        |> Seq.sortBy (fun issue -> System.DateTime.Now - issue.UpdatedAt)
+        |> Seq.truncate 5
+
+    for issue in topRecentlyUpdatedIssues do
+        printfn "#%d %s" issue.Number issue.Title
+
+    // #load "../packages/FSharp.Charting.0.90.9/FSharp.Charting.fsx";;
+    //World bank
+    type WorldBank = WorldBankDataProvider<Asynchronous=true>
+    let data = WorldBank.GetDataContext()
+    data.Countries.Sweden
+        .Indicators.``Population, total``
+        |> Async.RunSynchronously
+        |> Chart.Line
+       
